a0293423dd90eaeeeda3f76187c9353a48190769
Tony Messias <tonysm@hey.com>
2023-05-10 11:50:30 -0300

    Remove duplicated start

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 import './bootstrap';
 import './elements/turbo-echo-stream-tag';
-import './libs/turbo';
-import './libs/alpine';
-
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
-window.Alpine = Alpine;
-
-Alpine.plugin(focus);
-
-Alpine.start();
+import './libs';
